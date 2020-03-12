d5f1f1fea4985af95bf444afbe9dcc1b40868716
L3oneM <apoWebDevAcc@gmail.com>
2020-03-12 14:11:19 +0200

    added persistor

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,19 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import pagesReducer from './pages/pages.reducer';
 import searchReducer from './search/search.reducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['pagesState']
+};
 
 const rootReducer = combineReducers({
   pagesState: pagesReducer,
   filters: searchReducer
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
